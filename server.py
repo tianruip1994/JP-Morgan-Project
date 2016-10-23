388c638690af911b2f6811ccc5e6da2728e3134f
Jiangfeng Wang <jw3107@columbia.edu>
2016-10-23 18:36:24 -0400

    Modified server.py

--- server.py
+++ server.py
@@ -107,14 +107,4 @@ def signout():
     return redirect('/')
 
 if __name__ == "__main__":
-    import click
-
-    @click.command()
-    @click.option('--debug', is_flag=True)
-    @click.option('--threaded', is_flag=True)
-    @click.argument('HOST', default='0.0.0.0')
-    @click.argument('PORT', default=8111, type=int)
-    def run(debug, threaded, host, port):
-        HOST, PORT = host, port
-        app.run(host=HOST, port=PORT, debug=True, threaded=threaded)
-    run()
+    app.run()
